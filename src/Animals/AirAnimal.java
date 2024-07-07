4877f0eae904465da3fb8adb9333c1c2a45ae031
yaelD <yael3berko@gmail.com>
2024-07-07 16:28:53 +0300

    update

--- src/Animals/AirAnimal.java
+++ src/Animals/AirAnimal.java
@@ -1,4 +1,9 @@
 package Animals;
 
 public class AirAnimal extends Animal{
+    (x = 0,y = 100)
+    private double wingspan;
+    public AirAnimal(String name, enum gender, double weight, double speed, double wingspan){};
+
+
 }
--- src/Animals/Animal.java
+++ src/Animals/Animal.java
@@ -1,4 +1,24 @@
 package Animals;
+import Mobility.Mobile;
+import Olympics.Medal;
 
-public class Animal {
+public abstract class Animal extends Mobile{
+    private String name;
+    private enum gender;
+    private double weight,speed;
+    private Medal[]medals;
+    Point position;
+    protected abstract String getSound();
+
+    public Animal(String name, enum gender, double weight, double speed) {
+        this.name = name;
+        this.gender = gender;
+        this.weight = weight;
+        this.speed = speed;
+    }
+//    public void makeSound():type{
+        String st = "Animal" + name + "said" + getSound();
+//    }
+
+    
 }
