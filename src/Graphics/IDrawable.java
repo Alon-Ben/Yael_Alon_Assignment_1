ad81d9ce94aa5eeeeb0a4fb56cf5ec5026bcf24c
Alon <alon200053@gmail.com>
2024-07-15 14:39:31 +0300

    update IDrawable

--- src/Graphics/IDrawable.java
+++ src/Graphics/IDrawable.java
@@ -1,4 +1,10 @@
 package Graphics;
 
+import java.awt.*;
+
+
 public interface IDrawable {
+    public final static String PICTURE_PATH = "…";
+    public void loadImages(String nm);
+    public void drawObject (Graphics g);
 }
