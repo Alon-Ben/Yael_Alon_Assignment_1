0b425dc4026bce5ed99031f771973395569f162e
Alon <alon200053@gmail.com>
2024-07-17 15:12:11 +0300

    IReptile added TODO

--- src/Animals/IReptile.java
+++ src/Animals/IReptile.java
@@ -6,7 +6,7 @@ package Animals;
  */
 public interface IReptile {
     /** Maximum speed for reptiles in units per time (e.g., km/h) */
-    static final int MAX_SPEED = 5;
+    static final int MAX_SPEED = 5; // todo: public or protected?
 
     /**
      * Increases the speed of the reptile.
@@ -15,6 +15,7 @@ public interface IReptile {
      * @param increment The amount to increase the speed by
      * @return True if the speed was successfully increased, false otherwise
      *         (e.g., if the resulting speed would exceed MAX_SPEED)
+     *         todo: should we increase the speed up to MAX_SPEED yet return false for not being able to increase the "full" increment?
      */
     boolean speedUp(int increment);
 }
